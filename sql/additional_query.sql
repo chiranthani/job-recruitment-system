97caec3079e6949fa9a964aebe0dceebea9df879
Kavindya Gamage <kavindyagamage97@gmail.com>
2025-12-24 20:53:45 +0530

    post create feature implemented

--- sql/additional_query.sql
+++ sql/additional_query.sql
@@ -1,3 +1,5 @@
 /**  add additonal queries for any alter queries / inbulit insert queries  **/
 
-/**  date: **/
+/**  date:24/12/2025 **/
+
+ALTER TABLE `job_posts` ADD `post_status` ENUM('draft','published') NOT NULL DEFAULT 'draft' AFTER `requirements`;
